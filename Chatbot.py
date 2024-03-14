7bf9575e259863293291b700306539f1d11af308
AICrafter08 <chauhant08@gmail.com>
2024-03-14 22:56:12 +0530

    v4

--- Chatbot.py
+++ Chatbot.py
@@ -63,7 +63,10 @@ if prompt := st.chat_input(placeholder="Provide the data where biomarker is her2
             )
             response = st.write_stream(stream)
         message = {"role": "assistant", "content": response}
-        print(type(response), response , '\n', dir(response), len(response))
+        if len(response)>1:
+            response = response[-1]
+        else:
+            response = response
         sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
         with st.spinner('Getting insights from database based on the query.....'):
             if sql_match:
